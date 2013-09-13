c7008fd07904256d5e267533ca8e1e20839be858
Alex Bahm <alexgbahm@gmail.com>
2013-09-13 10:45:56 -0700

    Improved robustness, cleaned code, improved comments, easier to understand inputs.

--- PA4_2/main.cpp
+++ PA4_2/main.cpp
@@ -6,14 +6,16 @@
 int main(int argc, char* argv[])
 {
 	srand(time(NULL));
-	int arr_size = 5,
+	int arr_size = 0,
 		loops = 0,
 		newLoops = 0,
-		cap = 1000;
+		cap = 500,
+		intLength = -1;
 	char* tempchar =  new char();
-	char boolean = '\0';
 	string fileName,
-		tempStr;
+		tempStr,
+		input,
+		arr_input;
 	if(argv[1] != NULL)
 		fileName = argv[1];
 	
@@ -43,10 +45,7 @@ int main(int argc, char* argv[])
 		oldStart = NULL,
 		oldDone = NULL;
 
-cout << "Welcome to Maze Solving! "<< endl; //some introduction text because I was bored
-cout << " Created by: Alex Bahm" << endl;
-cout << " Cpts 223 WSU" << endl;
-cout << " April 2013" << endl << endl;
+
 //cout << "This code has been tested to work, however through multiple iterations of " << endl;
 //cout << "running, it has been known to crash. This only happens rarely. In addition," << endl;
 //cout << "occasionally maze generation will fail, simply try again and it should work" << endl << "fine. " << endl;
@@ -57,35 +56,32 @@ cout << endl;
 		//tStart = NULL; //resets the clock
 		 //I use a clock purely for curiosities sake
 		if(second)
-		{
+		{ //resets values to avoid errors
 			second = false;
 			loops = 0;
 			file = false;
 			stop = false;
-			clock_t tStart = NULL,
-			genStart = NULL,
-			dfsStart = NULL,
-			bfsStart = NULL,
-			oldStart = NULL,
-			oldDone = NULL;
-			theMaze = NULL;
-			mazeVector = NULL;
 		}
 		if(fileName.empty())
 		{
-			cout << "Do you want to import a maze? (T/F): ";
-			cin >> boolean;
+			cout << "What would you like to do? " << endl;
+			cout << "  I - To import a maze from a file" << endl;
+			cout << "  G - To generate a maze" << endl;
+			cout << "  O - To generate a maze with the obsolete algorithm" << endl;
+			cout << "  E - To exit " << endl << ">> ";
+			cin >> input;
 		}
-		//boolean = 't'; //for debugging
-		if(tolower(boolean) == 'e')
+		if(tolower(input[0]) == 'e')
 		{
 			exit(1);
 		}
-		else if(tolower(boolean) == 'o')
+		else if(tolower(input[0]) == 'o')
 		{
+			//Let's the system know to run the maze generation as the old algorithm so code doesn't have to be repeated.
+			cout << "Note: This code is obsolete and no longer supported. It is slow, and can rarely produce mazes greater than 12. It is included to show the improvement since spring." << endl;
 			old = true;
 		}
-		else if(tolower(boolean) == 't' || !fileName.empty())
+		else if(tolower(input[0]) == 'i' || !fileName.empty())
 		{
 			file = true;
 			if(fileName.empty())
@@ -93,11 +89,9 @@ cout << endl;
 				cout << "What is the file name? ";
 				cin >> fileName;
 			}
-			//fileName = "maze9.txt"; //also for debugging
-			genStart = clock();
-			tStart = clock();
+			genStart = tStart = clock();
 			mazeVector = readFile(fileName);
-			cout << "Read and maze generation time: " << double((clock() - genStart))/CLOCKS_PER_SEC << endl;
+			cout << "Maze rebuild time: " << double((clock() - genStart))/CLOCKS_PER_SEC << endl;
 			if(mazeVector == NULL)
 				theMaze = NULL;
 			else
@@ -106,36 +100,41 @@ cout << endl;
 				theMaze->printMaze(); //prints maze
 			}
 		}
-		if(tolower(boolean) == 'f' || tolower(boolean) == 'o')
+		if(tolower(input[0]) == 'g' || tolower(input[0]) == 'o')
 		{
 			do{
 				loops = 0;
-				if(arr_size < 2) // so we dont get an incredibly tiny maze, may cause some problem
+				if(arr_size == -100)
+					cout << "Please enter positive integer. " << endl;
+				else if(arr_size < 2) // so we dont get an incredibly tiny maze, may cause some problem
 					cout << "Please enter a larger number. " << endl;
-				else if(arr_size > cap) //I have trouble generating above 20 to begin with, so hene a cap
-					cout << "For the sake of your computer, please enter a smaller number. " << endl;
+				else if(arr_size > cap) //Further optimization is required to generate larger mazes
+					cout << "For the sake of your computer, please enter a number less than or equal to " << cap << "." << endl; //I should joke less in my code
 				cout << "How big of a maze would you like made? ";
-				cin >> arr_size;
+				cin >> arr_input;
+				for(intLength = 0; isdigit(arr_input[intLength]); intLength++);
+				if(intLength > 0)
+					arr_size = stoi(arr_input.substr(0, intLength));
+				else
+					arr_size = -100;
 			}while(arr_size < 2 || arr_size > cap);
 			cout << "Maze size: " << arr_size << " x " << arr_size << "; Nodes: " << arr_size * arr_size << endl;
-			genStart = clock();
-			tStart = clock();
+			genStart = tStart = clock();
 			cout << "Processing Time: " << endl;
 			if(!old)
-				cout << "\r" << (clock() - genStart)/CLOCKS_PER_SEC << flush;;
+				cout << "\r" << (clock() - genStart)/CLOCKS_PER_SEC << flush; //Keeps a rolling clock of the time taken to generate the maze
 			do{
-				if(!old)
+				if(!old) //Allows the user to compare the two algorithms
 					mazeVector = mazeGen(arr_size, genStart);
 				else
 					mazeVector = mazeGenOLD(arr_size);
-				loops++; //counts how many loops so it doesn't go on forever trying to achieve a maze
-				//cout << "."; //so users know the program has not entered an infinite loop
-			}while(mazeVector == NULL && loops < (3 * arr_size));
+				loops++; //I have noticed that random mazes can sometimes get stuck in generating, so I have learned that sometimes it is best to just restart
+			}while(mazeVector == NULL && loops < arr_size);
 
 			cout << endl;
 			genTime = double(clock() - genStart)/CLOCKS_PER_SEC;
 
-			if(loops >= (3 * arr_size))
+			if(loops >= arr_size)
 			{ //in case it takes too long generate, it stops trying
 				cout << endl << "Maze generation failed. Please try again or try a smaller maze size." << endl;
 				cout << "Maze generation attempt time: " << double(genTime)/CLOCKS_PER_SEC << endl;
@@ -147,44 +146,25 @@ cout << endl;
 				theMaze->printMaze();
 			}
 			
-			/*cout << "Testing old maze generation algorithm";
-			oldStart = clock();
-			newLoops = loops;
-			loops = 0;
-			do{
-				mazeVectorOLD = mazeGenOLD(arr_size);
-				loops++; //counts how many loops so it doesn't go on forever trying to achieve a maze
-				cout << "."; //so users know the program has not entered an infinite loop
-			}while(mazeVectorOLD == NULL && loops < (5 * newLoops));
-
-			if(loops >= (5 * newLoops))
-				cout << " - FAILED!!!" << endl;
-			else
-				cout << endl;
-
-			oldDone = clock() - oldStart;
-			cout << "Old Algorithm Time: " << double(oldDone)/CLOCKS_PER_SEC << endl;
-			cout << endl << "Difference: " << double(oldDone - genTime)/CLOCKS_PER_SEC << endl;*/
-
 		}
 		else
-			cout << "Please chose T/F next time." << endl;
+			cout << "Please enter a valid command next time." << endl;
 		if(theMaze != NULL)
 		{
 			bfsStart = clock();
-			theMaze->bfs(); //does the bfs algorithm on from file or random maze
+			theMaze->bfs(); //runs the bfs algorithm
 			bfsTime = double((clock() - bfsStart))/CLOCKS_PER_SEC;
 			cout << endl << endl;
+
 			dfsStart = clock();
-			theMaze->dfs(); //does the dfs algorithm
+			theMaze->dfs(); //runs the dfs algorithm
 			dfsTime = double((clock() - dfsStart))/CLOCKS_PER_SEC;			
 
 			if(arr_size > 40)
-			{
+			{ //If the maze is wider than the command line, I export it to a file for easier readability
 				
-				tempStr.append("Maze Size: ");
+				tempStr.append("Maze Size: "); //Formats the maze size string
 				itoa(arr_size, tempchar, 10);
-
 				tempStr.append(string(tempchar));
 				tempStr.append(" x ");
 				tempStr.append(string(tempchar));
@@ -194,7 +174,7 @@ cout << endl;
 				theMaze->fileWrite(tempStr);
 				tempStr.clear();
 
-				tempStr.append("Maze Generation Time: ");
+				tempStr.append("Maze Generation Time: "); //Formatting the times as strings so they can be included in the file
 				itoa(genTime, tempchar, 10);
 				tempStr.append(string(tempchar));
 				theMaze->fileWrite(tempStr);
@@ -214,12 +194,12 @@ cout << endl;
 			}
 
 			
-
+			//Either way, the time is presented to the user
 			cout << "Maze generation time: " << genTime << endl;
 			cout << "BFS sort time:        " << bfsTime << endl;
 			cout << "DFS sort time:        " << dfsTime << endl;
 			totTime = double((clock() - tStart))/CLOCKS_PER_SEC;
-			minutes = int(totTime) / 60;
+			minutes = int(totTime) / 60; //WIP
 			if(minutes > 60)
 				hours = int(minutes - 60) % 60;
 			else
@@ -239,7 +219,7 @@ cout << endl;
 				cout << "Total time (Seconds): " <<  totTime << endl << endl;
 			}
 		}
-		delete theMaze;
-		second = true;
+		delete theMaze; //Memory Management
+		second = true; //To check if values need to be reset or not.
 	}while(!stop);
 }
